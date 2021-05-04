747372e6227f287bcab465e198c64540c6d7f85b
keerthiinampudi <70694379+keerthiinampudi@users.noreply.github.com>
2021-05-04 14:39:30 +0530

    Add files via upload

---
+++ PROC41-Template-master/drop.js
@@ -0,0 +1,23 @@
+class createDrop{
+    constructor(x,y){
+        var options = {
+            restitution : 0.1,
+            friction :0.001
+        }
+        this.rain = Bodies.circle(x,y,5,options)
+        this.radius = 5
+        World.add(world,this.rain)
+    }
+    showDrop(){
+        var pos = this.rain.position
+        fill("blue")
+        ellipseMode(RADIUS)
+        ellipse(pos.x,pos.y,this.radius,this.radius)
+
+    }
+    updateY(){
+        if(this.rain.position.y>height){
+        Matter.Body.setPosition(this.rain,{x:random(0,400),y:random(0,400)})
+        }
+    }
+}
